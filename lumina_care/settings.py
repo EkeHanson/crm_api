348183294a682d39e33c76be60d0bf76b253d19b
support@prolianceltd.com <support@prolianceltd.com>
2025-06-23 13:16:04 +0100

    aloow all

--- lumina_care/settings.py
+++ lumina_care/settings.py
@@ -103,14 +103,14 @@ SOCIALACCOUNT_PROVIDERS = {
 }
 
 # Update CORS for OAuth redirects
-CORS_ALLOWED_ORIGINS = [
-    'http://app.mydomain.com',
-    'https://crm-frontend-react.vercel.app',
-    'http://localhost:5173',
-    'https://accounts.google.com',
-    'https://appleid.apple.com',
-    'https://login.microsoftonline.com',
-]
+# CORS_ALLOWED_ORIGINS = [
+#     'http://app.mydomain.com',
+#     'https://crm-frontend-react.vercel.app',
+#     'http://localhost:5173',
+#     'https://accounts.google.com',
+#     'https://appleid.apple.com',
+#     'https://login.microsoftonline.com',
+# ]
 CORS_ALLOW_CREDENTIALS = True
 CORS_ALLOW_METHODS = [
     'DELETE',

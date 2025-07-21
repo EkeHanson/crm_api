f87e2cdec7315316c4bc4c1d511267c17beb022f
support@prolianceltd.com <support@prolianceltd.com>
2025-07-21 21:44:10 +0100

    getting it cors

--- lumina_care/settings.py
+++ lumina_care/settings.py
@@ -106,8 +106,8 @@ CORS_ALLOWED_ORIGINS = [
     'http://localhost:5173',
 ]
 
-# CORS_ALLOW_CREDENTIALS = True
 
+CORS_ALLOW_CREDENTIALS = True
 CORS_ALLOW_METHODS = ['DELETE', 'GET', 'OPTIONS', 'PATCH', 'POST', 'PUT']
 CORS_ALLOW_HEADERS = ['accept', 'authorization', 'content-type', 'origin', 'x-csrftoken', 'x-requested-with']
 
